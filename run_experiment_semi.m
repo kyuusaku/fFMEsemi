b0f0c773cb6814a60346d3e309823c7eb86034f6
Suo <q.suo@foxmail.com>
2017-07-11 00:30:30 +0800

    use the same anchor

--- run_experiment_semi.m
+++ run_experiment_semi.m
@@ -94,7 +94,7 @@ end
 %% compute efficient anchor graph
 eag_data = fullfile(save_path, 'eag.mat');
 if ~exist(eag_data, 'file')
-    [~, anchor, kmeans_time] = k_means(X_train, para.num_anchor);
+    %[~, anchor, kmeans_time] = k_means(X_train, para.num_anchor);
     Z = cell(numel(para.beta), 1);
     rLz = cell(numel(para.beta), 1);
     eag_time = zeros(numel(para.beta), 1);
