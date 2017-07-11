e33b9bac0286199fbca95a26bcaf755523e8f9dd
Suo <q.suo@foxmail.com>
2017-07-11 22:55:21 +0800

    run more experiments

--- main.m
+++ main.m
@@ -3,3 +3,11 @@ clear;
 parpool(12);
 %%
 run_experiment_semi('norb',1)
+%%
+run_experiment_semi('norb',5)
+%%
+run_experiment_semi('norb',8)
+%%
+run_experiment_semi('norb',10)
+%%
+run_experiment_semi('norb',15)
