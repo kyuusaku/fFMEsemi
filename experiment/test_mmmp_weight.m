c3c436d75b46de26db56577e50f18a6dc5efcc02
Suo Qiu <q.suo@foxmail.com>
2017-08-18 03:16:18 +0800

    update mmmp_weight

--- experiment/test_mmmp_weight.m
+++ experiment/test_mmmp_weight.m
@@ -1,6 +1,10 @@
 %% test mmmp_weight
 
-% input
+%% 
+addpath('../fFME');
+addpath('../mmlp');
+
+%% input
 E = [0,1,0,2,0,0,0,0,0;
      1,0,1,1,2,0,0,0,0;
      0,1,0,2,1,2,0,0,0;
@@ -16,7 +20,7 @@ aIdx = [3;4;5;9];
 
 k = 2;
 
-% answer
+%% answer
 answer_D = [1,1,1,inf;
             1,1,1,inf;
             0,1,1,inf;
@@ -25,15 +29,16 @@ answer_D = [1,1,1,inf;
             2,2,2,inf;
             inf,inf,inf,2;
             inf,inf,inf,2;
-            inf,inf,inf,0];
+            inf,inf,inf,0]
 
 [val, pos] = sort(answer_D, 2);
 
 val = val(:,1:k);
 pos = pos(:,1:k);
 
-val = val ./ (max(val, 2) * ones(1,k));
+val = val ./ (max(val, [], 2) * ones(1,k));
 val = exp(-val);
+val(isnan(val)) = 0;
 val = val ./ (sum(val, 2) * ones(1,k));
 
 n = size(E,1);
@@ -41,7 +46,7 @@ m = size(aIdx,1);
 answer = sparse(reshape(repmat(1:n, k,1), 1,[]), reshape(pos', 1,[]), reshape(val', 1,[]), n, m);
 answer = full(answer)
 
-% output
-[Z, elapsed_time, D] = mmmp_weight(E, [], aIdx, [], k);
+%% output
+[Z, elapsed_time, D] = mmmp_weight(E, aIdx, k);
 D
 Z = full(Z)
--- fFME/mmmp_weight.m
+++ fFME/mmmp_weight.m
@@ -1,28 +1,21 @@
-function [Z, elapsed_time, D] = mmmp_weight(E, anchor, aIdx, X_train, k)
-
-if isempty(aIdx)
-    % obtain aIdx
-    [index, search_params] = flann_build_index(X_train, ...
-        struct('algorithm', 'kdtree', 'trees', 8, 'checks', 128));
-    search_params.cores = 0;
-    aIdx = flann_search(index, anchor, 1, search_params);
-end
+function [Z, elapsed_time, D] = mmmp_weight(E, aIdx, k)
 
 t_start = tic;
 % 
-n = size(X_train, 2);
-m = size(anchor, 2);
+n = size(E, 1);
+m = numel(aIdx);
 
 D = zeros(n, m); % init minmax distance matrix
 
-parfor i = 1 : m
+for i = 1 : m
     y_pred = zeros(n, 1);
     y_pred(aIdx(i)) = i;
     
     d = inf(n, 1);
     d(aIdx(i)) = 0;
 
-    [num_iter, size_Q, iters] = mmlp_core(aIdx(i), E, y_pred, d, 0.9999);
+    [num_iter, size_Q, iters] = my_mmlp_core(aIdx(i), E, y_pred, d);%, 0.9999);
+    fprintf('%d/%d num_iter:%d\n', i, m, num_iter);
     
     D(:, i) = d;
 end
@@ -32,8 +25,9 @@ end
 val = val(:,1:k);
 pos = pos(:,1:k);
 
-val = val ./ (max(val, 2) * ones(1,k));
+val = val ./ (max(val, [], 2) * ones(1,k));
 val = exp(-val);
+val(isnan(val)) = 0;
 val = val ./ (sum(val, 2) * ones(1,k));
 
 Z = sparse(reshape(repmat(1:n, k,1), 1,[]), reshape(pos', 1,[]), reshape(val', 1,[]), n, m);
