b6ee61e05fa5a5bbe57646784dd12a608c66b5c8
Suo <q.suo@foxmail.com>
2017-08-26 12:51:32 +0800

    update run gau test for aFME ..

--- testcode/run_two_gaussian_test.m
+++ testcode/run_two_gaussian_test.m
@@ -14,14 +14,19 @@ d = 1;
 std = 0.01:0.005:0.07;
 
 agr_train = zeros(numel(std),1);
+eagr_train = zeros(numel(std),1);
 mmlp_train = zeros(numel(std),1);
 mtc_train = zeros(numel(std),1);
 nn_train = zeros(numel(std),1);
 laprls_train = zeros(numel(std),1);
 ffme_train = zeros(numel(std),1);
+effme_train = zeros(numel(std),1);
+afme_train = zeros(numel(std),1);
 nn_test = zeros(numel(std),1);
 laprls_test = zeros(numel(std),1);
 ffme_test = zeros(numel(std),1);
+effme_test = zeros(numel(std),1);
+afme_test = zeros(numel(std),1);
 
 save_path = 'result/test/semi';
 for i = 1:numel(std)
@@ -29,8 +34,14 @@ for i = 1:numel(std)
     
     ffme_data_1_1e9_para_best = fullfile(record_path, 'result_fastFME1_1e9_para_best2.mat');
     load(ffme_data_1_1e9_para_best);
+    effme_data_1e9_para_best = fullfile(record_path, 'result_efFME_1e9_para_best.mat');
+    load(effme_data_1e9_para_best);
+    afme_data_1e9_para_best = fullfile(record_path, 'result_aFME_1e9_para_best.mat');
+    load(afme_data_1e9_para_best);
     agr_data_para_best = fullfile(record_path, 'result_AGR_para_best.mat');
     load(agr_data_para_best);
+    eagr_data_para_best = fullfile(record_path, 'result_EAGR_para_best.mat');
+    load(eagr_data_para_best);
     mmlp_data_para = fullfile(record_path, 'result_MMLP_min_para.mat');
     load(mmlp_data_para);
     mmlp_data_para = fullfile(record_path, 'result_MMLP_max_para.mat');
@@ -48,26 +59,34 @@ for i = 1:numel(std)
     load(laprls_data2_para_best);
     
     agr_train(i) = result_AGR_para_best{1}.best_train_accuracy(1);
+    eagr_train(i) = result_EAGR_para_best{1}.best_train_accuracy(1);
     mmlp_train(i) = result_MMLP_para{1}.best_train_accuracy(1);
     mtc_train(i) = result_MTC_para{1}.best_train_accuracy(1);
     nn_train(i) = result_NN_para{1}.best_train_accuracy(1);
     laprls_train(i) = result_LapRLS2_para_best{1}.best_train_accuracy(1);
     ffme_train(i) = result_fastFME1_1e9_para_best{1}.best_train_accuracy(1);
+    effme_train(i) = result_efFME_1e9_para_best{1}.best_train_accuracy(1);
+    afme_train(i) = result_aFME_1e9_para_best{1}.best_train_accuracy(1);
     nn_test(i) = result_NN_para{1}.best_test_accuracy(1);
     laprls_test(i) = result_LapRLS2_para_best{1}.best_test_accuracy(1);
     ffme_test(i) = result_fastFME1_1e9_para_best{1}.best_test_accuracy(1);
+    effme_train(i) = result_efFME_1e9_para_best{1}.best_test_accuracy(1);
+    afme_train(i) = result_aFME_1e9_para_best{1}.best_test_accuracy(1);
 end
 
 h=figure(1);
 m_size = 10;
 subplot(1,2,1);
-plot(std,agr_train, '--c*', 'MarkerSize',m_size);hold on;
+plot(std,agr_train, '-.c', 'MarkerSize',m_size);hold on;
+plot(std,eagr_train, '--c*', 'MarkerSize',m_size);hold on;
 plot(std,mmlp_train, '--m<', 'MarkerSize',m_size);hold on;
 plot(std,mtc_train, '--m>', 'MarkerSize',m_size);hold on;
 plot(std,nn_train, '--ko', 'MarkerSize',m_size);hold on;
 plot(std,laprls_train, '--bs', 'MarkerSize',m_size);hold on;
-plot(std,ffme_train, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
-legend('AGR','MMLP','MTC','NN','LapRLS/L','f-FME',...
+%plot(std,ffme_train, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+plot(std,effme_train, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+plot(std,afme_train, '-r^', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+legend('AGR','EAGR','MMLP','MTC','NN','LapRLS/L','f-FME','a-FME',...
     'Location','SouthWest');
 set(gca,'FontSize',14);
 xlabel('variance of the distribution');
@@ -75,8 +94,10 @@ ylabel('Unlabel accuracy');
 subplot(1,2,2);
 plot(std,nn_test, '--ko', 'MarkerSize',m_size);hold on;
 plot(std,laprls_test, '--bs', 'MarkerSize',m_size);hold on;
-plot(std,ffme_test, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
-legend('NN','LapRLS/L','f-FME',...
+%plot(std,ffme_test, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+plot(std,effme_test, '-rd', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+plot(std,afme_test, '-r^', 'LineWidth', 2, 'MarkerSize',m_size);hold on;
+legend('NN','LapRLS/L','f-FME','a-FME',...
     'Location','SouthWest');
 %set(gca, 'XTick', std);
 %set(gca, 'XTickLabel', {'3,750','7,500','15,000','30,000','60,000'});
@@ -88,4 +109,4 @@ ylabel('Test accuracy');
 % print(h, '..\\..\\t.epsc', '-depsc');
 
 %% 
-min(mnistk32graph, [], 1);
+%min(mnistk32graph, [], 1);
