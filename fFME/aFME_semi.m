ff0150f913001cfe2091573716c642cc5ab9cc46
kyuusaku <q.suo@foxmail.com>
2018-02-06 21:11:12 +0800

    check aFME_semi right

--- fFME/aFME_semi.m
+++ fFME/aFME_semi.m
@@ -2,7 +2,7 @@ function [W, b, F] = aFME_semi(X, Z, rLz, Y, para)
 % INPUT
 %     X: f*m matrix, each colomn is a data point
 %     Z: n*m matrix, sample adaptive weights
-%     rLz: reduced 
+%     rLz: m*m matrix, reduced normalized graph
 %     Y: n*c matrix, class indicator matrix. 
 %        Yij=1 if xi is labeled as j, Yij=0 otherwise
 %     para: parameters
@@ -11,9 +11,9 @@ function [W, b, F] = aFME_semi(X, Z, rLz, Y, para)
 %       para.mu: the parameter
 %       para.gamma: the parameter
 % OUTPUT
-%     W: projection matrix
-%     b: bias vector
-%     F: soft label matrix
+%     W: f*c projection matrix
+%     b: c*1 bias vector
+%     F: n*c soft label matrix
 
 [dim,m] = size(X);
 
@@ -26,7 +26,7 @@ u(sum(Y,2) == 1) = para.ul;
 U = spdiags(u,0,n,n);
 
 A = (rLz + Z'*U*Z + para.mu*eye(m) - (para.mu/m)*ones(m,1)*ones(1,m) ...
-    - para.mu*Xc'*W) \ (Z'*U*Y);
+    - para.mu*Xc'*W) \ (Z'*U*Y); % A is a m*c matrix
 
 F = Z*A;
 W = W*A;
