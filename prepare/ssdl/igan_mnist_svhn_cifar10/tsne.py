104ae6b8a3c5052508c472c6a137c8fd6f616286
Suo <q.suo@foxmail.com>
2018-02-26 02:04:46 +0800

    add tsne

---
+++ prepare/ssdl/igan_mnist_svhn_cifar10/tsne.py
@@ -0,0 +1,22 @@
+import scipy.io as sio
+from sklearn.manifold import TSNE
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', type=str)
+args = parser.parse_args()
+print(args)
+
+# read data
+data = sio.loadmat(args.data_path + '/fea.mat')
+trainx = data['trainx']
+trainy = data['trainy']
+testx = data['testx']
+testy = data['testy']
+
+# TSNE
+trainx_embedded = TSNE(n_components=2).fit_transform(trainx)
+testx_embedded = TSNE(n_components=2).fit_transform(testx)
+
+scipy.io.savemat(save_path + '/fea_tsne.mat', 
+                 mdict={'trainx': trainx_embedded, 'trainy': trainy,
+                        'testx': testx_embedded, 'testy': testy})
