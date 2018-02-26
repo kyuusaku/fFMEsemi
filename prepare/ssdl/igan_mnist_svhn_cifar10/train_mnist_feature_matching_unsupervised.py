2d40bdc66ce8334353d5c294deb1e676a929f431
Suo <q.suo@foxmail.com>
2018-02-26 15:14:33 +0800

    update

--- prepare/ssdl/igan_mnist_svhn_cifar10/train_mnist_feature_matching_unsupervised.py
+++ prepare/ssdl/igan_mnist_svhn_cifar10/train_mnist_feature_matching_unsupervised.py
@@ -19,7 +19,7 @@ parser.add_argument('--seed_data', type=int, default=1)
 parser.add_argument('--unlabeled_weight', type=float, default=1.)
 parser.add_argument('--batch_size', type=int, default=100)
 parser.add_argument('--count', type=int, default=10)
-parser.add_argument('--save_path', type=str, default='output/mnist_feature_matching')
+parser.add_argument('--save_path', type=str, default='output/mnist_feature_matching_unsupervised')
 args = parser.parse_args()
 print(args)
 
@@ -61,35 +61,21 @@ layers.append(nn.GaussianNoiseLayer(layers[-1], sigma=0.5))
 layers.append(nn.DenseLayer(layers[-1], num_units=10, nonlinearity=None, train_scale=True))
 
 # costs
-labels = T.ivector()
-x_lab = T.matrix()
 x_unl = T.matrix()
 
 temp = LL.get_output(gen_layers[-1], init=True)
-temp = LL.get_output(layers[-1], x_lab, deterministic=False, init=True)
 init_updates = [u for l in gen_layers+layers for u in getattr(l,'init_updates',[])]
 
-output_before_softmax_lab = LL.get_output(layers[-1], x_lab, deterministic=False)
 output_before_softmax_unl = LL.get_output(layers[-1], x_unl, deterministic=False)
 output_before_softmax_fake = LL.get_output(layers[-1], gen_dat, deterministic=False)
 
-z_exp_lab = T.mean(nn.log_sum_exp(output_before_softmax_lab))
-z_exp_unl = T.mean(nn.log_sum_exp(output_before_softmax_unl))
-z_exp_fake = T.mean(nn.log_sum_exp(output_before_softmax_fake))
-l_lab = output_before_softmax_lab[T.arange(args.batch_size),labels]
 l_unl = nn.log_sum_exp(output_before_softmax_unl)
 loss_lab = -T.mean(l_lab) + T.mean(z_exp_lab)
 loss_unl = -0.5*T.mean(l_unl) + 0.5*T.mean(T.nnet.softplus(nn.log_sum_exp(output_before_softmax_unl))) + 0.5*T.mean(T.nnet.softplus(nn.log_sum_exp(output_before_softmax_fake)))
 
-train_err = T.mean(T.neq(T.argmax(output_before_softmax_lab,axis=1),labels))
-
 mom_gen = T.mean(LL.get_output(layers[-3], gen_dat), axis=0)
 mom_real = T.mean(LL.get_output(layers[-3], x_unl), axis=0)
 loss_gen = T.mean(T.square(mom_gen - mom_real))
-
-# test error
-output_before_softmax = LL.get_output(layers[-1], x_lab, deterministic=True)
-test_err = T.mean(T.neq(T.argmax(output_before_softmax,axis=1),labels))
 
 # Theano functions for training and testing
 lr = T.scalar()
@@ -100,10 +86,9 @@ disc_avg_updates = [(a,a+0.0001*(p-a)) for p,a in zip(disc_params,disc_param_avg
 disc_avg_givens = [(p,a) for p,a in zip(disc_params,disc_param_avg)]
 gen_params = LL.get_all_params(gen_layers[-1], trainable=True)
 gen_param_updates = nn.adam_updates(gen_params, loss_gen, lr=lr, mom1=0.5)
-init_param = th.function(inputs=[x_lab], outputs=None, updates=init_updates)
-train_batch_disc = th.function(inputs=[x_lab,labels,x_unl,lr], outputs=[loss_lab, loss_unl, train_err], updates=disc_param_updates+disc_avg_updates)
+init_param = th.function(inputs=[x_unl], outputs=None, updates=init_updates)
+train_batch_disc = th.function(inputs=[x_unl,lr], outputs=[loss_unl], updates=disc_param_updates+disc_avg_updates)
 train_batch_gen = th.function(inputs=[x_unl,lr], outputs=[loss_gen], updates=gen_param_updates)
-test_batch = th.function(inputs=[x_lab,labels], outputs=test_err, givens=disc_avg_givens)
 
 # load MNIST data
 data = np.load('mnist.npz')
@@ -115,18 +100,6 @@ nr_batches_train = int(trainx.shape[0]/args.batch_size)
 testx = data['x_test'].astype(th.config.floatX)
 testy = data['y_test'].astype(np.int32)
 nr_batches_test = int(testx.shape[0]/args.batch_size)
-
-# select labeled data
-inds = data_rng.permutation(trainx.shape[0])
-trainx = trainx[inds]
-trainy = trainy[inds]
-txs = []
-tys = []
-for j in range(10):
-    txs.append(trainx[trainy==j][:args.count])
-    tys.append(trainy[trainy==j][:args.count])
-txs = np.concatenate(txs, axis=0)
-tys = np.concatenate(tys, axis=0)
 
 trainx_permutation = trainx.copy()
 trainy_permutation = trainy.copy()
@@ -154,28 +127,18 @@ for epoch in range(300):
     trainx_unl2 = trainx_unl2[rng.permutation(trainx_unl2.shape[0])]
 
     # train
-    loss_lab = 0.
     loss_unl = 0.
-    train_err = 0.
+    loss_gen = 0.
     for t in range(nr_batches_train):
-        ll, lu, te = train_batch_disc(trainx[t*args.batch_size:(t+1)*args.batch_size],trainy[t*args.batch_size:(t+1)*args.batch_size],
-                                        trainx_unl[t*args.batch_size:(t+1)*args.batch_size],lr)
-        loss_lab += ll
+        lu = train_batch_disc(trainx_unl[t*args.batch_size:(t+1)*args.batch_size],lr)
         loss_unl += lu
-        train_err += te
         e = train_batch_gen(trainx_unl2[t*args.batch_size:(t+1)*args.batch_size],lr)
-    loss_lab /= nr_batches_train
+        loss_gen += e
     loss_unl /= nr_batches_train
-    train_err /= nr_batches_train
-
-    # test
-    test_err = 0.
-    for t in range(nr_batches_test):
-        test_err += test_batch(testx[t*args.batch_size:(t+1)*args.batch_size],testy[t*args.batch_size:(t+1)*args.batch_size])
-    test_err /= nr_batches_test
+    loss_gen /= nr_batches_train
 
     # report
-    print("Iteration %d, time = %ds, loss_lab = %.4f, loss_unl = %.4f, train err = %.4f, test err = %.4f" % (epoch, time.time()-begin, loss_lab, loss_unl, train_err, test_err))
+    print("Iteration %d, time = %ds, loss_unl = %.4f" % (epoch, time.time()-begin, loss_unl))
     sys.stdout.flush()
 
 # save trained model
