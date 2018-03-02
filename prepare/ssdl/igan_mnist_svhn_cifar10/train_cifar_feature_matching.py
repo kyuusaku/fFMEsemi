efed4e5f007c464fccd4107151130d863e3586a5
Suo <q.suo@foxmail.com>
2018-03-02 17:04:28 +0800

    fix np random type error

--- prepare/ssdl/igan_mnist_svhn_cifar10/train_cifar_feature_matching.py
+++ prepare/ssdl/igan_mnist_svhn_cifar10/train_cifar_feature_matching.py
@@ -18,10 +18,10 @@ import cifar10_data
 
 # settings
 parser = argparse.ArgumentParser()
-parser.add_argument('--seed', default=1)
-parser.add_argument('--seed_data', default=1)
-parser.add_argument('--count', default=400)
-parser.add_argument('--batch_size', default=100)
+parser.add_argument('--seed', type=int, default=1)
+parser.add_argument('--seed_data', type=int, default=1)
+parser.add_argument('--count', type=int, default=400)
+parser.add_argument('--batch_size', type=int, default=100)
 parser.add_argument('--unlabeled_weight', type=float, default=1.)
 parser.add_argument('--learning_rate', type=float, default=0.0003)
 parser.add_argument('--data_dir', type=str, default='cifar-10-python')
