a844c991f6479fc5afff416c17b5ae7f9d88265f
kyuusaku <q.suo@foxmail.com>
2018-03-08 05:09:41 +0800

    add draw anchors

---
+++ testcode/draw_anchors.m
@@ -0,0 +1,56 @@
+%% draw anchors
+
+%%
+dataset = 'usps';
+num_anchors = [50, 100 : 100 : 1000];
+FME_anchor = 100;
+
+%%
+dataset = 'coil100';
+num_anchors = 200 : 200 : 4000;
+FME_anchor = 100;
+
+%%
+save_path = ['result/' dataset '/'];
+
+load([save_path 'semi-a' num2str(FME_anchor) '/record-10/result_FME1_1_para_best.mat']); 
+FME_train = result_FME1_1_para_best{1,1}.best_train_accuracy;
+FME_test = result_FME1_1_para_best{1,1}.best_test_accuracy;
+
+fFME_train = zeros(numel(num_anchors), 2);
+fFME_test = zeros(numel(num_anchors), 2);
+aFME_train = zeros(numel(num_anchors), 2);
+aFME_test = zeros(numel(num_anchors), 2);
+for i = 1:numel(num_anchors)
+    record_path = [save_path 'semi-a' num2str(num_anchors(i)) '/record-10/'];
+    load([record_path 'result_fastFME1_1e9_para_best2.mat']);
+    fFME_train(i,:) = result_fastFME1_1e9_para_best{1,1}.best_train_accuracy;
+    fFME_test(i,:) = result_fastFME1_1e9_para_best{1,1}.best_test_accuracy;
+    load([record_path 'result_aFME_1e9_para_best.mat']);
+    aFME_train(i,:) = result_aFME_1e9_para_best{1,1}.best_train_accuracy;
+    aFME_test(i,:) = result_aFME_1e9_para_best{1,1}.best_test_accuracy;
+end
+
+save([save_path 'data.mat'],'FME_train','FME_test','fFME_train','fFME_test','aFME_train','aFME_test');
+%%
+load([save_path 'data.mat']);
+
+h1=figure(1);
+m_size = 10;
+f_size = 16;
+
+plot(num_anchors, repmat(FME_train(1),numel(num_anchors),1), '-k'); hold on;
+plot(num_anchors, repmat(FME_test(1),numel(num_anchors),1), '--k'); hold on;
+plot(num_anchors, fFME_train(:,1), '-r^', 'MarkerSize', m_size); hold on;
+plot(num_anchors, fFME_test(:,1), '--r^', 'MarkerSize', m_size); hold on;
+plot(num_anchors, aFME_train(:,1), '-mp', 'MarkerSize', m_size); hold on;
+plot(num_anchors, aFME_test(:,1), '--mp', 'MarkerSize', m_size); hold on;
+
+legend('FME unlabel','FME test','f-FME unlabel','f-FME test','a-FME unlabel','a-FME test',...
+    'Location','SouthEast');
+set(gca,'YLim',[30,100]);
+set(gca,'XLim',[min(num_anchors),max(num_anchors)]);
+xlabel('Number of anchors','FontSize',f_size);
+ylabel('Unlabel & Test accuracy','FontSize',f_size);
+set(gca,'FontSize',f_size);
+print(h1,'-dpng',fullfile(save_path, 'anchors.png'));
