9eef0fb735f99123da5619c83d073d3d6db63151
Suo <q.suo@foxmail.com>
2018-03-12 19:13:11 +0800

    add ssdl cifar regression

---
+++ prepare/ssdl/igan_mnist_svhn_cifar10/train_cifar_feature_matching_regression.py
@@ -0,0 +1,237 @@
+import os
+import scipy.io
+from utils import save_model
+import argparse
+import time
+import numpy as np
+import theano as th
+import theano.tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams
+import lasagne
+import lasagne.layers as ll
+from lasagne.init import Normal
+from lasagne.layers import dnn
+import nn
+import sys
+import plotting
+import cifar10_data
+
+# settings
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', type=int, default=1)
+parser.add_argument('--seed_data', type=int, default=1)
+parser.add_argument('--count', type=int, default=400)
+parser.add_argument('--batch_size', type=int, default=100)
+parser.add_argument('--unlabeled_weight', type=float, default=1.)
+parser.add_argument('--learning_rate', type=float, default=0.0003)
+parser.add_argument('--data_dir', type=str, default='cifar-10-python')
+parser.add_argument('--save_path', type=str, default='output/cifar_feature_matching')
+args = parser.parse_args()
+print(args)
+
+save_path = os.getcwd() + '/' + args.save_path + '/' \
+          + 'seed' + str(args.seed) \
+          + '_seeddata' + str(args.seed_data) \
+          + '_count' + str(args.count)
+if not os.path.isdir(save_path):
+    print('make dir')
+    os.mkdir(save_path)
+
+# fixed random seeds
+rng_data = np.random.RandomState(args.seed_data)
+rng = np.random.RandomState(args.seed)
+theano_rng = MRG_RandomStreams(rng.randint(2 ** 15))
+lasagne.random.set_rng(np.random.RandomState(rng.randint(2 ** 15)))
+
+# load CIFAR-10
+trainx, trainy = cifar10_data.load(args.data_dir, subset='train')
+trainx_unl = trainx.copy()
+trainx_unl2 = trainx.copy()
+testx, testy = cifar10_data.load(args.data_dir, subset='test')
+nr_batches_train = int(trainx.shape[0]/args.batch_size)
+nr_batches_test = int(testx.shape[0]/args.batch_size)
+
+# specify generative model
+noise_dim = (args.batch_size, 100)
+noise = theano_rng.uniform(size=noise_dim)
+gen_layers = [ll.InputLayer(shape=noise_dim, input_var=noise)]
+gen_layers.append(nn.batch_norm(ll.DenseLayer(gen_layers[-1], num_units=4*4*512, W=Normal(0.05), nonlinearity=nn.relu), g=None))
+gen_layers.append(ll.ReshapeLayer(gen_layers[-1], (args.batch_size,512,4,4)))
+gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,256,8,8), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 4 -> 8
+gen_layers.append(nn.batch_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,128,16,16), (5,5), W=Normal(0.05), nonlinearity=nn.relu), g=None)) # 8 -> 16
+gen_layers.append(nn.weight_norm(nn.Deconv2DLayer(gen_layers[-1], (args.batch_size,3,32,32), (5,5), W=Normal(0.05), nonlinearity=T.tanh), train_g=True, init_stdv=0.1)) # 16 -> 32
+gen_dat = ll.get_output(gen_layers[-1])
+
+# specify discriminative model
+disc_layers = [ll.InputLayer(shape=(None, 3, 32, 32))]
+disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.2))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 96, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=1, stride=2, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(ll.DropoutLayer(disc_layers[-1], p=0.5))
+disc_layers.append(nn.weight_norm(dnn.Conv2DDNNLayer(disc_layers[-1], 192, (3,3), pad=0, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(nn.weight_norm(ll.NINLayer(disc_layers[-1], num_units=192, W=Normal(0.05), nonlinearity=nn.lrelu)))
+disc_layers.append(ll.GlobalPoolLayer(disc_layers[-1]))
+disc_layers.append(nn.weight_norm(ll.DenseLayer(disc_layers[-1], num_units=10, W=Normal(0.05), nonlinearity=None), train_g=True, init_stdv=0.1))
+disc_params = ll.get_all_params(disc_layers, trainable=True)
+
+# costs
+labels = T.ivector()
+label_matrix = T.matrix()
+x_lab = T.tensor4()
+x_unl = T.tensor4()
+temp = ll.get_output(gen_layers[-1], deterministic=False, init=True)
+temp = ll.get_output(disc_layers[-1], x_lab, deterministic=False, init=True)
+init_updates = [u for l in gen_layers+disc_layers for u in getattr(l,'init_updates',[])]
+
+output_lab = ll.get_output(disc_layers[-1], x_lab, deterministic=False)
+output_unl = ll.get_output(disc_layers[-1], x_unl, deterministic=False)
+output_gen = ll.get_output(disc_layers[-1], gen_dat, deterministic=False)
+
+loss_lab = T.mean(T.sum(T.pow(output_lab - label_matrix, 2), axis=1)) # Squared Error
+l_gen = T.mean(T.sum(T.pow(output_gen, 2), axis=1)) # L2 norm
+log_fx = output_unl - nn.log_sum_exp(output_unl).dimshuffle(0,'x')
+loss_entropy = T.mean(T.sum(T.exp(log_fx) * log_fx, axis=1)) # Entropy loss
+loss_unl = -0.5*loss_entropy + 0.5*l_gen
+
+train_err = T.mean(T.neq(T.argmax(output_lab,axis=1),labels))
+
+# test error
+output_test = ll.get_output(disc_layers[-1], x_lab, deterministic=True)
+test_err = T.mean(T.neq(T.argmax(output_test,axis=1),labels))
+
+# Theano functions for training the disc net
+lr = T.scalar()
+disc_params = ll.get_all_params(disc_layers, trainable=True)
+disc_param_updates = nn.adam_updates(disc_params, loss_lab + args.unlabeled_weight*loss_unl, lr=lr, mom1=0.5)
+disc_param_avg = [th.shared(np.cast[th.config.floatX](0.*p.get_value())) for p in disc_params]
+disc_avg_updates = [(a,a+0.0001*(p-a)) for p,a in zip(disc_params,disc_param_avg)]
+disc_avg_givens = [(p,a) for p,a in zip(disc_params,disc_param_avg)]
+init_param = th.function(inputs=[x_lab], outputs=None, updates=init_updates) # data based initialization
+train_batch_disc = th.function(inputs=[x_lab,labels,label_matrix,x_unl,lr], outputs=[loss_lab, loss_unl, train_err], updates=disc_param_updates+disc_avg_updates)
+test_batch = th.function(inputs=[x_lab,labels], outputs=test_err, givens=disc_avg_givens)
+samplefun = th.function(inputs=[], outputs=gen_dat)
+
+# Theano functions for training the gen net
+output_unl = ll.get_output(disc_layers[-2], x_unl, deterministic=False)
+output_gen = ll.get_output(disc_layers[-2], gen_dat, deterministic=False)
+m1 = T.mean(output_unl,axis=0)
+m2 = T.mean(output_gen,axis=0)
+loss_gen = T.mean(abs(m1-m2)) # feature matching loss
+gen_params = ll.get_all_params(gen_layers, trainable=True)
+gen_param_updates = nn.adam_updates(gen_params, loss_gen, lr=lr, mom1=0.5)
+train_batch_gen = th.function(inputs=[x_unl,lr], outputs=None, updates=gen_param_updates)
+
+
+# generate 
+#x = T.matrix()
+#output_before_classifier = ll.get_output(disc_layers[-2], x, deterministic=True)
+generate_feature = th.function(inputs=[x_unl], outputs=output_unl)
+
+# select labeled data
+inds = rng_data.permutation(trainx.shape[0])
+trainx = trainx[inds]
+trainy = trainy[inds]
+txs = []
+tys = []
+for j in range(10):
+    txs.append(trainx[trainy==j][:args.count])
+    tys.append(trainy[trainy==j][:args.count])
+txs = np.concatenate(txs, axis=0)
+tys = np.concatenate(tys, axis=0)
+
+tys_matrix = np.zeros((tys.shape[0], 10))
+tys_matrix[np.arange(tys.shape[0]), tys] = 1
+
+trainx_permutation = trainx.copy()
+trainy_permutation = trainy.copy()
+scipy.io.savemat(save_path + '/data.mat', 
+                 mdict={'trainx': trainx_permutation, 'trainy': trainy_permutation,
+                        'testx': testx, 'testy': testy})
+
+# //////////// perform training //////////////
+for epoch in range(1200):
+    begin = time.time()
+    lr = np.cast[th.config.floatX](args.learning_rate * np.minimum(3. - epoch/400., 1.))
+
+    # construct randomly permuted minibatches
+    trainx = []
+    trainy = []
+    trainy_matrix = []
+    for t in range(int(np.ceil(trainx_unl.shape[0]/float(txs.shape[0])))):
+        inds = rng.permutation(txs.shape[0])
+        trainx.append(txs[inds])
+        trainy.append(tys[inds])
+        trainy_matrix.append(tys_matrix[inds])
+    trainx = np.concatenate(trainx, axis=0)
+    trainy = np.concatenate(trainy, axis=0)
+    trainy_matrix = np.concatenate(trainy_matrix, axis=0)
+    trainx_unl = trainx_unl[rng.permutation(trainx_unl.shape[0])]
+    trainx_unl2 = trainx_unl2[rng.permutation(trainx_unl2.shape[0])]
+    
+    if epoch==0:
+        print(trainx.shape)
+        init_param(trainx[:500]) # data based initialization
+
+    # train
+    loss_lab = 0.
+    loss_unl = 0.
+    train_err = 0.
+    for t in range(nr_batches_train):
+        ran_from = t*args.batch_size
+        ran_to = (t+1)*args.batch_size
+        ll, lu, te = train_batch_disc(trainx[ran_from:ran_to],trainy[ran_from:ran_to],trainy_matrix[ran_from:ran_to],
+                                      trainx_unl[ran_from:ran_to],lr)
+        loss_lab += ll
+        loss_unl += lu
+        train_err += te
+        
+        train_batch_gen(trainx_unl2[t*args.batch_size:(t+1)*args.batch_size],lr)
+
+    loss_lab /= nr_batches_train
+    loss_unl /= nr_batches_train
+    train_err /= nr_batches_train
+    
+    # test
+    test_err = 0.
+    for t in range(nr_batches_test):
+        test_err += test_batch(testx[t*args.batch_size:(t+1)*args.batch_size],testy[t*args.batch_size:(t+1)*args.batch_size])
+    test_err /= nr_batches_test
+
+    # report
+    print("Iteration %d, time = %ds, loss_lab = %.4f, loss_unl = %.4f, train err = %.4f, test err = %.4f" % (epoch, time.time()-begin, loss_lab, loss_unl, train_err, test_err))
+    sys.stdout.flush()
+
+    # generate samples from the model
+    sample_x = samplefun()
+    img_bhwc = np.transpose(sample_x[:100,], (0, 2, 3, 1))
+    img_tile = plotting.img_tile(img_bhwc, aspect_ratio=1.0, border_color=1.0, stretch=True)
+    img = plotting.plot_img(img_tile, title='CIFAR10 samples')
+    plotting.plt.savefig(save_path+"/cifar_sample_feature_match.png")
+    plotting.plt.close()
+
+    # save params
+    #np.savez('disc_params.npz', *[p.get_value() for p in disc_params])
+    #np.savez('gen_params.npz', *[p.get_value() for p in gen_params])
+
+
+# save trained model
+save_model(save_path + '/final', disc_layers)
+# save features
+fea_trainx = np.zeros((trainx_permutation.shape[0], 192))
+fea_testx = np.zeros((testx.shape[0], 192))
+for t in range(nr_batches_train):
+    fea_trainx[t*args.batch_size:(t+1)*args.batch_size] = generate_feature(trainx_permutation[t*args.batch_size:(t+1)*args.batch_size])
+    print("Generate features for train set: %d", t)
+for t in range(nr_batches_test):
+    fea_testx[t*args.batch_size:(t+1)*args.batch_size] = generate_feature(testx[t*args.batch_size:(t+1)*args.batch_size])
+    print("Generate features for test set: %d", t)
+scipy.io.savemat(save_path + '/fea.mat', 
+                 mdict={'trainx': fea_trainx, 'trainy': trainy_permutation,
+                        'testx': fea_testx, 'testy': testy})
+
