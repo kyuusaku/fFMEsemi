ec750df28e72bcea232133b9fc2f785aaa849d8b
Suo <q.suo@foxmail.com>
2018-03-12 20:48:49 +0800

    update

--- prepare/ssdl/igan_mnist_svhn_cifar10/train_cifar_feature_matching_regression.py
+++ prepare/ssdl/igan_mnist_svhn_cifar10/train_cifar_feature_matching_regression.py
@@ -93,9 +93,9 @@ output_lab = ll.get_output(disc_layers[-1], x_lab, deterministic=False)
 output_unl = ll.get_output(disc_layers[-1], x_unl, deterministic=False)
 output_gen = ll.get_output(disc_layers[-1], gen_dat, deterministic=False)
 
-loss_lab = T.sum(T.sum(T.pow(output_lab - label_matrix, 2), axis=1)) # Squared Error
-l_gen = T.sum(T.sum(T.pow(output_gen, 2), axis=1)) # L2 norm
-l_unl = T.sum(T.sum(output_unl * T.log(output_unl), axis=1))
+loss_lab = T.mean(T.sum(T.pow(output_lab - label_matrix, 2), axis=1)) # Squared Error
+l_gen = T.mean(T.sum(T.pow(output_gen, 2), axis=1)) # L2 norm
+l_unl = T.mean(T.sum(output_unl * T.log(output_unl), axis=1))
 loss_unl = -0.5*l_unl + 0.5*l_gen
 """
 log_gen = output_gen - nn.log_sum_exp(output_gen).dimshuffle(0,'x')
